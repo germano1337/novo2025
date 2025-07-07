515cf9da56a1bdec23fabbb415ef4289f7ad1d09
germano1337 <germano13@hotmail.com>
2025-07-07 16:52:33 -0300

    novo commit

--- teste1.py
+++ teste1.py
@@ -3,3 +3,9 @@ n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 media = (n1 + n2) / 2
 print(f'A média é: {media:.1f}')
+
+'''verifica se a média é maior ou igual a 7.0'''
+if media >= 7.0:
+    print('Aprovado')
+else:
+    print('Reprovado')
